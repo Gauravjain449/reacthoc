849998b45e2c7fa7863c265dd59a32341ed7f7f5
Jain <g.h.jain@accenture.com>
2019-05-13 10:36:24 +0530

    local commit

--- A_Node/File2.js
+++ A_Node/File2.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
     res.send("Hello Guarav Jain !");
 });
 
+app.route('/node').get(function (req, res) {
+    res.send('Node Request Called!');
+});
+
 var server = app.listen(3000, function () {
     console.log('Server Start!');
 });
