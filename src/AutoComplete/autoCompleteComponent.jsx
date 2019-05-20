e73773019ffe20cf20510b4b4162d756ff71ac4c
Jain <g.h.jain@accenture.com>
2019-05-20 12:50:38 +0530

    commit

--- src/AutoComplete/autoCompleteComponent.jsx
+++ src/AutoComplete/autoCompleteComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import data from '../data/datasource.json';
-
+import './autoComplete.css'
 
 class AutoCompleteComponent extends Component {
     constructor(props) {
@@ -13,28 +13,24 @@ class AutoCompleteComponent extends Component {
     }
     onSearchHandler(e) {
         let { filterData, searchtext } = this.state;
-        let _filterData = [];
+        filterData = [];
         searchtext = e.target.value;
-        const _data = (search) => {
-            return data.booksData.filter((i, index) => {
-                retrun(i.BookName.indexOf(search) > -1);
-
-
-            })
-        }
-        console.log(_data);
-        this.setState({ searchtext }, () => {
-            _data(searchtext);
+        filterData = data.booksData.filter((data) => {
+            return data.BookName.toLowerCase().startsWith(searchtext);
+        }).slice(0, 10);
+        this.setState({
+            searchtext, filterData
         });
-
     }
     render() {
         return (
             <div>
                 <input type="text" value={this.state.searchtext} onChange={this.onSearchHandler} />
-                {/* {this.state.filterData.map((item) => {
-                    return <div key={item}>{item}</div>
-                })} */}
+                {this.state.filterData.length > 0 && <div><ul className="ul-without-bullet">
+                    {this.state.filterData.map((item) => {
+                        return <li key={item.BookID}>{item.BookName}</li>
+                    })}
+                </ul></div>}
             </div>
         )
     }
